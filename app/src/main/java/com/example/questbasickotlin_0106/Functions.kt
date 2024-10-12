30b98fdfa37cd15e0d2da1b31859c21fb02bd52d
Bayhaqiindra <bayhaqi30072004.com>
2024-10-12 22:21:37 +0700

    Menambahkan fungsi withParameter yang menerima parameter name dan mencetak pesan sapaan

--- app/src/main/java/com/example/questbasickotlin_0106/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin_0106/Functions.kt
@@ -4,3 +4,8 @@ fun withoutParameter() {
     println(" == withoutParameter == ")
     println("Hello, World!")
 }
+fun withParameter(name: String) {
+    println()
+    println(" == withParameter == ")
+    println("Hello, $name!")
+}
